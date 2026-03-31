cccdd1d962aecb10dc43305acc9fbe02540a2bd8
PhoenixBrasseaux8249995 <ppb8249995@lpssonline.com>
2026-03-31 15:54:48 +0000

    todo 9 complete

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -74,15 +74,7 @@ for (var i = 0; i < circles.length; i++) {
 }
                       
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-game.checkCirclePosition(circles[ 0 ])
 
-game.checkCirclePosition(circles[ 1 ])
-
-game.checkCirclePosition(circles[ 2 ])
-
-game.checkCirclePosition(circles[ 3 ])
-
-game.checkCirclePosition(circles[ 4 ])
              // TODO 9 : Iterate over the array
            
             
