660284e7f47aec4b2ea3bb219926b89e353113d4
PhoenixBrasseaux8249995 <ppb8249995@lpssonline.com>
2026-03-31 15:49:44 +0000

    todo 2 complete

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -15,115 +15,66 @@ var init = function (window) {
         window.opspark.game = {};
         var game = window.opspark.game;
         
-        ///////////////////
-        // PROGRAM SETUP //
-        ///////////////////
+        ////////////////////////////////////////////////////////////
+        ///////////////// PROGRAM SETUP ////////////////////////////
+        ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-var circle
-        var circles  = []
-
-
+var circle 
+var circles=[]
+      
         // TODO 2 : Create a function that draws a circle 
-        function drawCircle(){
-           circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
+       function drawCircle(){
+        circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
 
               physikz.addRandomVelocity(circle, canvas, 2, 2);
 
               view.addChild(circle);
 
-              circles.push(circle); 
-        }
+              circles.push(circle);
+       }
+       
 
+        // TODO 3 / 7 : Call the drawCircle() function 
 
-        // TODO 3 : Call the drawCircle() function
-drawCircle()
-
-drawCircle()
-
-drawCircle()
-
-drawCircle()
-
-drawCircle()
-
-
-        // TODO 7 : Use a loop to create multiple circles
-drawCircle()
-
-drawCircle()
-
-drawCircle()
-
-drawCircle()
-
-drawCircle()
-for (var loopsCompleted = 0; loopsCompleted < 175; loopsCompleted++) {
-
-            drawCircle();
-
-        }
-
-
-        ///////////////////
-        // PROGRAM LOGIC //
-        ///////////////////
+        
+        ////////////////////////////////////////////////////////////
+        ///////////////// PROGRAM LOGIC ////////////////////////////
+        ////////////////////////////////////////////////////////////
         
         /* 
-        This Function is called 60 times/second, producing 60 frames/second.
+        This Function is called 60 times/second producing 60 frames/second.
         In each frame, for every circle, it should redraw that circle
         and check to see if it has drifted off the screen.         
         */
         function update() {
-            // TODO 4 : Update the position of each circle using physikz.updatePosition()
-for (var i = 0; i < circles.length; i++) {
+            // TODO 4 : Update the circle's position //
 
-               physikz.updatePosition(circles[i]);
+                      
+            // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
 
-               game.checkCirclePosition(circles[i]);
-
-
-}
-
-
-            
-            // TODO 5 : Call game.checkCirclePosition() on your circles
-           
-
-            // TODO 8 / TODO 9 : Iterate over the array
+             // TODO 9 : Iterate over the array
            
             
-        
+
     
         /* 
         This Function should check the position of a circle that is passed to the 
         Function. If that circle drifts off the screen, this Function should move
         it to the opposite side of the screen.
         */
+
         game.checkCirclePosition = function(circle) {
 
             // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
-          
-            
-            
+                      if(circle.x > canvas.width){
+  circle.x = 0
+}
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-else if (circle.x < 0) {
-
-                circle.x = canvas.width;
-
-            } else if (circle.y > canvas.height) {
-
-                circle.y = 0;
-
-            } else if (circle.y < 0) {
-
-                circle.y = canvas.height;
-
-            }            
-
-
+            
+           
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
-        
+        }
         
         /////////////////////////////////////////////////////////////
         // --- NO CODE BELOW HERE  --- DO NOT REMOVE THIS CODE --- //
@@ -141,9 +92,10 @@ else if (circle.x < 0) {
     }
 };
 
-// DO NOT REMOVE THIS CODE //////////////////////////////////////////////////////
+// DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
 if((typeof process !== 'undefined') &&
     (typeof process.versions.node !== 'undefined')) {
     // here, export any references you need for tests //
     module.exports = init;
 }
+
